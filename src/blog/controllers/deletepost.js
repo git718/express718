da279229c9e072565fa5d573bfcfcf04564eddaf
Anton718 <anton.kostroma@yahoo.com>
2024-05-29 07:14:35 -0400

    add

--- src/blog/controllers/deletepost.js
+++ src/blog/controllers/deletepost.js
@@ -1,4 +1,10 @@
 exports.deleteposts = async (req, res) => {
-  await db.query("DELETE FROM posts WHERE id=$1", [req.params.id]);
-  res.redirect("/blog");
+  try {
+    await db.query("DELETE FROM posts WHERE id=$1", [req.params.id]);
+    res.redirect("/blog");
+  }
+  catch {
+    res.redirect("/blog");
+  }
+  
 };
