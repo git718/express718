4b528eda3b1b93e4dc45f842c8f5559077e07efb
Anton718 <anton.kostroma@yahoo.com>
2024-05-29 20:02:00 -0400

    add

--- src/messages/controllers/send_private2.js
+++ src/messages/controllers/send_private2.js
@@ -2,6 +2,9 @@ exports.sendPrivMessage2 = async (req, res) => {
   const token = req.signedCookies.token;
   if (token) {
     if (req.body.private) {
+      if (req.body.private.length > 150) {
+        req.body.private = req.body.private.slice(0, 150)
+      }
       await db.query(
         "INSERT INTO private(to_user, from_user, content) VALUES ($1, $2, $3)",
         [req.body.to_user, req.body.from_user, req.body.private]
