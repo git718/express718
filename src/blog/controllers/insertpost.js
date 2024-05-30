dc0e8f4e187e40f413506a83d038c4d9d499d5e3
Anton718 <anton.kostroma@yahoo.com>
2024-05-30 19:59:18 -0400

    add

--- src/blog/controllers/insertpost.js
+++ src/blog/controllers/insertpost.js
@@ -2,8 +2,8 @@ exports.insertposts = async (req, res) => {
   const token = req.signedCookies.token;
   if (token) {
     const user = jwt.verify(token, "rwervterbj353jhbdkfhv");
-    if (req.body.post.length > 1000) {
-      req.body.post = req.body.post.slice(0, 1000)
+    if (req.body.post.length > 20000) {
+      req.body.post = req.body.post.slice(0, 20000)
     }
     await db.query("INSERT INTO posts(content, username) VALUES ($1, $2)", [
       req.body.post,
--- src/messages/controllers/send_private.js
+++ src/messages/controllers/send_private.js
@@ -11,8 +11,8 @@ exports.sendPrivMessage = async (req, res) => {
       image: image[0].image,
     };
     if (req.body.private) {
-      if (req.body.private.length > 1000) {
-        req.body.private = req.body.private.slice(0, 1000)
+      if (req.body.private.length > 20000) {
+        req.body.private = req.body.private.slice(0, 20000)
       }
         await db.query(
           "INSERT INTO private(to_user, from_user, content) VALUES ($1, $2, $3)",
