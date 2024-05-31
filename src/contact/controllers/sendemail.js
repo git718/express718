04e405ae1cc77805b5f97b11966ff45a5ed8c165
Anton718 <anton.kostroma@yahoo.com>
2024-05-31 07:35:49 -0400

    add

--- src/contact/controllers/sendemail.js
+++ src/contact/controllers/sendemail.js
@@ -1,50 +1,31 @@
 exports.mailer = async (req, res) => {
-  console.log(req.body.email, req.body.textarea)
-  if (req.body.email!="" && 
-  req.body.textarea!="" && 
-  req.body.email.includes("@")) {
-    if (req.body.email.length > 80) {
-      req.body.email = req.body.email.slice(0, 80)
-    }
-    if (req.body.textarea.length > 20000) {
-      req.body.textarea = req.body.textarea.slice(0, 20000)
-    }
-  
-  await db.query("INSERT INTO messages(email, text) VALUES ($1, $2)", [
-    req.body.email,
-    req.body.textarea,
-  ]);
-  const token = req.signedCookies.token;
-  if (token) {
-    const user = jwt.verify(token, "rwervterbj353jhbdkfhv");
-    return res.render("form", { 
-      active: "", 
-      token: token, 
-      user: user.username 
-    });
-  } else {
-    return res.render("form", { 
-      active: "",
-      token: "", 
-      user: "" });
-  }
-  
-} else {
+    if (req.body.email && 
+    req.body.textarea && 
+    req.body.email.includes("@")) {
+      if (req.body.email.length > 80) {
+        req.body.email = req.body.email.slice(0, 80)
+      }
+      if (req.body.textarea.length > 20000) {
+        req.body.textarea = req.body.textarea.slice(0, 20000)
+      }
+    
+    await db.query("INSERT INTO messages(email, text) VALUES ($1, $2)", [
+      req.body.email,
+      req.body.textarea,
+    ]);
     const token = req.signedCookies.token;
     if (token) {
       const user = jwt.verify(token, "rwervterbj353jhbdkfhv");
-      return res.render("contacts", { 
-        active: "contacts", 
-        message:"message not sent",
+      res.render("form", { 
+        active: "", 
         token: token, 
         user: user.username 
       });
     } else {
-      return res.render("contacts", { 
-        active: "contacts",
+      res.render("form", { 
+        active: "",
         token: "", 
-        message:"message not sent",
         user: "" });
     }
-  };
+  }
 }
