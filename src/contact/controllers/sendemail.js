7b1cc2cdb26c7aea9715c68c631184659fcbc89e
Anton718 <anton.kostroma@yahoo.com>
2024-05-31 07:04:32 -0400

    add

--- src/contact/controllers/sendemail.js
+++ src/contact/controllers/sendemail.js
@@ -11,7 +11,6 @@ exports.mailer = async (req, res) => {
     req.body.email,
     req.body.textarea,
   ]);
-}
   const token = req.signedCookies.token;
   if (token) {
     const user = jwt.verify(token, "rwervterbj353jhbdkfhv");
@@ -24,6 +23,24 @@ exports.mailer = async (req, res) => {
     res.render("form", { 
       active: "",
       token: "", 
+      user: "" });
+  }
+  
+}
+  const token = req.signedCookies.token;
+  if (token) {
+    const user = jwt.verify(token, "rwervterbj353jhbdkfhv");
+    res.render("contacts", { 
+      active: "contacts", 
+      message:"message not sent",
+      token: token, 
+      user: user.username 
+    });
+  } else {
+    res.render("contacts", { 
+      active: "contacts",
+      token: "", 
+      message:"message not sent",
       user: "" });
   }
 };
