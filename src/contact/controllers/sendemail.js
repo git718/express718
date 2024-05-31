6a22f4d07dec02634dfb00a87ba209abeb4829c9
Anton718 <anton.kostroma@yahoo.com>
2024-05-31 06:45:13 -0400

    add

--- src/contact/controllers/sendemail.js
+++ src/contact/controllers/sendemail.js
@@ -1,10 +1,12 @@
 exports.mailer = async (req, res) => {
+  if (req.body.email && req.body.textarea) {
     if (req.body.email.length > 80) {
       req.body.email = req.body.email.slice(0, 80)
     }
     if (req.body.textarea.length > 20000) {
       req.body.textarea = req.body.textarea.slice(0, 20000)
     }
+  }
   await db.query("INSERT INTO messages(email, text) VALUES ($1, $2)", [
     req.body.email,
     req.body.textarea,
