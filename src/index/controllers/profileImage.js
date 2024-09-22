3fde7e00d2cc25ccdbe68d50f2a92afed81bb266
git718 <anton.kostroma@yahoo.com>
2024-09-22 07:40:33 -0400

    add

--- src/index/controllers/profileImage.js
+++ src/index/controllers/profileImage.js
@@ -10,7 +10,7 @@ exports.addImage = async (req, res) => {
       )}`;
       req.files.image.mv("public/uploads/" + fileName);
       sharp("public/uploads/" + fileName).resize(600,600)
-      .jpeg({quality : 80}).toFile("public/uploads/" + fileName);
+      .jpeg({quality : 80}).toFile("public/uploads/" + "resized_" + fileName);
 
       let imagePath = `uploads/${fileName}`;
       await db.query("UPDATE users SET image = $1 WHERE name = $2", [
