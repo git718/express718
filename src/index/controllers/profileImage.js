6203fbd73f75a41d6ca7daec7227e3cdf4bbc978
git718 <anton.kostroma@yahoo.com>
2024-09-23 07:33:27 -0400

    add

--- src/index/controllers/profileImage.js
+++ src/index/controllers/profileImage.js
@@ -18,7 +18,7 @@ exports.addImage = async (req, res) => {
       req.query.userData? req.query.userData.toLowerCase():null,
     ]);
     if (req.files) {
-      let extensions = ["JPEG", "jpeg", "GIF", "gif", "PNG", "png", ".JPEG", ".jpeg", ".GIF", ".gif", ".PNG", ".png"]
+      let extensions = ["JPEG", "jpeg", "GIF", "gif", "PNG", "png", ".JPEG", ".jpeg", ".GIF", ".gif", ".PNG", ".png", "JPG", ".JPG", ".jpg", "jpg"]
       for (let i of extensions) {
           if (path.extname(req.files.image.name) == i) {
           
@@ -40,7 +40,7 @@ exports.addImage = async (req, res) => {
         } else {
           res.render("signin", {
             active: "signin",
-            response: "Please use image format PNG, JPEG, or GIF.",
+            response: "Please use image format PNG, JPEG,JPG or GIF.",
             token: token,
             yourBio: bio[0].bio,
             image: userImage[0].image,
