0badc05dfb04945ce4cd76b5f7cb4e4c1936516d
git718 <anton.kostroma@yahoo.com>
2024-09-23 07:19:52 -0400

    add

--- src/index/controllers/profileImage.js
+++ src/index/controllers/profileImage.js
@@ -5,22 +5,34 @@ exports.addImage = async (req, res) => {
   if (token) {
     const user = jwt.verify(token, "rwervterbj353jhbdkfhv");
     if (req.files) {
-      let fileName = `${Math.random() * 10000000000000000}${path.extname(
-        req.files.image.name
-      )}`;
-      await req.files.image.mv("public/uploads/" + fileName);
-      await sharp("./public/uploads/" + fileName).resize(600,600)
-      .toFormat('jpeg')
-      .rotate()
-      .toFile("./public/uploads/" + "resized_" + fileName);
-
-      let imagePath = `uploads/${"resized_" + fileName}`;
-      await db.query("UPDATE users SET image = $1 WHERE name = $2", [
-        imagePath,
-        user.username,
-      ]);
+      let extensions = ["JPEG", "jpeg", "GIF", "gif", "PNG", "png", ".JPEG", ".jpeg", ".GIF", ".gif", ".PNG", ".png"]
+      for (let i of extensions) {
+          if (path.extname(req.files.image.name) == i) {
+          
+          let fileName = `${Math.random() * 10000000000000000}${path.extname(
+            req.files.image.name
+          )}`;
+          await req.files.image.mv("public/uploads/" + fileName);
+          await sharp("./public/uploads/" + fileName).resize(600,600)
+          .toFormat('jpeg')
+          .rotate()
+          .toFile("./public/uploads/" + "resized_" + fileName);
+        
+          let imagePath = `uploads/${"resized_" + fileName}`;
+          await db.query("UPDATE users SET image = $1 WHERE name = $2", [
+            imagePath,
+            user.username,
+          ]);
+          res.redirect("signin");
+        } else {
+          res.render("signin", {
+            active: "signin",
+            response: "Please use image format PNG, JPEG, or GIF.",
+            token: "",
+          });
+        }
+      } 
     }
-    res.redirect("signin");
   }
 };
 
