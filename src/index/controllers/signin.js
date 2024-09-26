a2e2347feb5926e72c8d63d5b33331472a6ad27c
git718 <anton.kostroma@yahoo.com>
2024-09-26 07:12:27 -0400

    add

--- src/index/controllers/signin.js
+++ src/index/controllers/signin.js
@@ -12,7 +12,7 @@ exports.signin = async (req, res) => {
       "SELECT * FROM private WHERE to_user = $1 ORDER by id DESC",
       [user.username]
     );
-    const userData = await db.query("SELECT * FROM users WHERE name LIKE $1", [
+    const userData = await db.query("SELECT * FROM users WHERE name LIKE %$1%", [
       req.query.userData? req.query.userData.toLowerCase():null,
     ]);
 
