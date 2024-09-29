80446fc37e6edd2f2f9bbfb49aa3e3078c2d96ab
git718 <anton.kostroma@yahoo.com>
2024-09-29 06:58:18 -0400

    add

--- src/blog/blog.routes.js
+++ src/blog/blog.routes.js
@@ -3,11 +3,14 @@ const router = express.Router();
 const getposts = require("./controllers/getposts");
 const deleteposts = require("./controllers/deletepost");
 const insertposts = require("./controllers/insertpost");
+const fileUpload = require("express-fileupload");
 
 router.get("/blog", urlencodedParser, getposts.getposts);
 
 router.get("/blog/:id", deleteposts.deleteposts);
 
 router.post("/post", urlencodedParser, insertposts.insertposts);
+router.post("/post", fileUpload(), insertposts.insertposts);
+
 
 module.exports = router;
