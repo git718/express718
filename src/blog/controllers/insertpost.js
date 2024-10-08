e4448a81f2227c3e65508d9a5ce248451208081f
git718 <anton.kostroma@yahoo.com>
2024-10-07 21:47:39 -0400

    add

--- src/blog/controllers/insertpost.js
+++ src/blog/controllers/insertpost.js
@@ -6,41 +6,38 @@ exports.insertposts = async (req, res) => {
     const user = jwt.verify(token, "rwervterbj353jhbdkfhv");
   
   
-    if (req.fields) {
+    if (req.fields || req.files) {
+
       if (req.fields?.post?.length > 20000) {
       req.fields.post = req.fields.post.slice(0, 20000)
     }
-    await db.query("INSERT INTO posts(content, username) VALUES ($1, $2)", [
+
+  let imagePath = null;
+
+    if (req.files) {
+      let extensions = [".JPEG", ".jpeg", ".GIF", ".gif", ".PNG", ".png", ".JPG", ".jpg"]
+      for (let i of extensions) {
+          if (extensions.includes(path.extname(req.files.image.name))) {
+          let fileName = `${Math.random() * 10000000000000000}${path.extname(
+            req.files.image.name
+          )}`;
+          await sharp(req.files.image.path).resize(600,600)
+          .toFile("./public/uploads/" + "resized_" + fileName);
+        
+          imagePath = "/uploads/resized_" + fileName;
+        } 
+      } 
+    }
+
+
+    await db.query("INSERT INTO posts(content, username, uploads) VALUES ($1, $2, $3)", [
       req.fields.post,
       user.username,
+      imagePath
     ]);
   }
-
-if (req.files) {
-  console.log(req.files.image.name);
-  
-  let extensions = [".JPEG", ".jpeg", ".GIF", ".gif", ".PNG", ".png", ".JPG", ".jpg"]
-  for (let i of extensions) {
-      if (extensions.includes(path.extname(req.files.image.name))) {
-      let fileName = `${Math.random() * 10000000000000000}${path.extname(
-        req.files.image.name
-      )}`;
-      await sharp(req.files.image.path).resize(600,600)
-      .toFile("./public/uploads/" + "resized_" + fileName);
-    
-      let imagePath = "/uploads/resized_" + fileName;
-    console.log(imagePath);
-
-      await db.query("INSERT INTO posts(uploads) VALUES ($1)", [
-        imagePath,
-      ]);
-      return res.redirect("blog");
-    } else { return res.redirect("blog")}
-  } 
-}
     
   return res.redirect("blog");
-
 
   } else {
     response = "Войдите в свой аккаунт или зарегистрируйте пользователя.";
