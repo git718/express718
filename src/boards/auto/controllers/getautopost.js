72af00975bf5d7460f8495b4ed7992f541f69dc3
git718 <anton.kostroma@yahoo.com>
2025-02-02 15:56:08 -0500

    add

--- src/boards/auto/controllers/getautopost.js
+++ src/boards/auto/controllers/getautopost.js
@@ -2,14 +2,14 @@ exports.getautopost = async (req, res) => {
     const token = req.signedCookies.token;
     if (token) {
       const user = jwt.verify(token, "rwervterbj353jhbdkfhv");
-      return res.render("autopost", {
+      return res.render("postauto", {
         active: "boards",
         response: "",
         token: token,
         user: user.username,
       });
     } else {
-      res.render("autopost", {
+      res.render("postauto", {
         active: "boards",
         response: "",
         token: "",
