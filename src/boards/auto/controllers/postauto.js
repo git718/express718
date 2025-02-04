6147ea7598abc84cd20ec6aec5875e28e420798f
git718 <anton.kostroma@yahoo.com>
2025-02-03 20:57:45 -0500

    add

--- src/boards/auto/controllers/postauto.js
+++ src/boards/auto/controllers/postauto.js
@@ -3,13 +3,12 @@ exports.postauto = async (req, res) => {
     if (token) {
       const user = jwt.verify(token, "rwervterbj353jhbdkfhv");
       const user_id = await db.query("SELECT id from users WHERE name=$1", [user.username])
-      console.log(user_id);
       
       if (req.fields.username && req.fields.brand && req.fields.model && 
         req.fields.year && req.fields.amount
       ) {
         await db.query("INSERT INTO auto(user_id, username, make, model, year, description) VALUES ($1, $2, $3, $4, $5, $6)",
-            [user_id.rows[0].id, req.fields.username, req.fields.brand, req.fields.model, req.fields.year, req.fields.description])
+            [user_id[0].id, req.fields.username, req.fields.brand, req.fields.model, req.fields.year, req.fields.description])
             return res.redirect("auto")
       }
       return res.render("postauto", {
