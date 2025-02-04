8430ceed052e1984f7d76a53fe478ed97c3a4ac0
git718 <anton.kostroma@yahoo.com>
2025-02-03 20:15:24 -0500

    add

--- src/boards/auto/controllers/postauto.js
+++ src/boards/auto/controllers/postauto.js
@@ -8,14 +8,14 @@ exports.postauto = async (req, res) => {
       ) {
         await db.query("INSERT INTO auto(user_id, username, make, model, year, description) VALUES ($1, $2, $3, $4, $5, $6)",
             [user_id, req.body.username, req.body.make, req.body.model, req.body.year, req.body.description])
-            return res.render("auto", {
+            return res.render("postauto", {
                 active: "boards",
-                response: "",
+                response: "added your post",
                 token: token,
                 user: user.username,
               });
       }
-      return res.render("auto", {
+      return res.render("postauto", {
         active: "boards",
         response: "Some fields need to be filled.",
         token: token,
