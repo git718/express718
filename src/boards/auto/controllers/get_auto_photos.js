88120e41894b39a9f2cd82226e3a7e83320f7feb
Anton718 <anton.kostroma@yahoo.com>
2025-02-08 22:12:56 -0500

    add

--- src/boards/auto/controllers/get_auto_photos.js
+++ src/boards/auto/controllers/get_auto_photos.js
@@ -1,34 +1,14 @@
 exports.getautophotos = async (req, res) => {
     const token = req.signedCookies.token
     const id = req.params.post_id
-    const autos = await db.query("SELECT * FROM auto ORDER BY post_id DESC")
-    const users = await db.query("SELECT * FROM users")
     const object = await db.query("SELECT * FROM auto WHERE post_id=$1", 
         [id]
     )
     const links = object[0].photo
     if (links == null || links == './public/images/car.png') {
-      if (token) {
-        const user = jwt.verify(token, "rwervterbj353jhbdkfhv")
-        return res.render("auto", {
-          active: "boards",
-          response: "no photos from seller",
-          token: token,
-          user: user.username,
-          autos: autos,
-          users: users
-        });
-      } else {
-        return res.render("auto", {
-          active: "boards",
-          response: "no photo from seller. register to message sellers",
-          token: "",
-          user: "",
-          autos: autos,
-          users: users
-        });
-      }
-    }
+        return res.redirect("/auto")
+      } 
+
     const splitted_links = links.split(',')
     const sorted_links = splitted_links.filter(element => element !== '' && element !== null)
     
