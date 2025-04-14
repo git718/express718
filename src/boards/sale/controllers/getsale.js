c070277b6381b4f7e16725158643925f87c56711
Anton <112634784+git718@users.noreply.github.com>
2025-04-14 15:30:53 -0400

    Update getsale.js

--- src/boards/sale/controllers/getsale.js
+++ src/boards/sale/controllers/getsale.js
@@ -1,7 +1,8 @@
 exports.getsale = async (req, res) => {
     const token = req.signedCookies.token;
     if (token) {
-      const user = jwt.verify(token, "rwervterbj353jhbdkfhv");
+      const user = jwt.verify(token, process.env.JWT_SECRET);
+
       return res.render("sale", {
         active: "boards",
         response: "",
